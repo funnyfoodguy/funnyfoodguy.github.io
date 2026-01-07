ba51f84c0957da7673cdfe025cc209da8a5be33c
funnyfoodguy <funnyfoodguy@gmail.com>
2026-01-07 03:20:36 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,7 +9,7 @@ document.addEventListener("click", function() {
   };
 });
 
-document.addEventListener("touchend", function() {
+window.addEventListener("touchstart", function() {
   if (document.body.style.overflow !== 'hidden' && document.URL === 'https://funnyfoodguy.com'){
     document.body.style.overflow = 'hidden';
   };
