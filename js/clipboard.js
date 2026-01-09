b8f86c64e02b8919a5166cb44e822b1b807c8f4c
funnyfoodguy <funnyfoodguy@gmail.com>
2026-01-09 16:03:51 -0500

    Create clipboard.js

---
+++ js/clipboard.js
@@ -0,0 +1,406 @@
+const copyBtn1 = document.querySelector('.copy-btn1');
+const text1 = document.querySelector('.text1');
+function copy1() {
+    navigator.clipboard.writeText(text1.innerText);
+
+    copyBtn1.innerText = 'Copied';
+    text1.classList.add('copied');
+
+    setTimeout(() => {
+        text1.classList.remove('copied');
+        copyBtn1.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn2 = document.querySelector('.copy-btn2');
+const text2 = document.querySelector('.text2');
+function copy2() {
+    navigator.clipboard.writeText(text2.innerText);
+
+    copyBtn2.innerText = 'Copied';
+    text2.classList.add('copied');
+
+    setTimeout(() => {
+        text2.classList.remove('copied');
+        copyBtn2.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn3 = document.querySelector('.copy-btn3');
+const text3 = document.querySelector('.text3');
+function copy3() {
+    navigator.clipboard.writeText(text3.innerText);
+
+    copyBtn3.innerText = 'Copied';
+    text3.classList.add('copied');
+
+    setTimeout(() => {
+        text3.classList.remove('copied');
+        copyBtn3.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn4 = document.querySelector('.copy-btn4');
+const text4 = document.querySelector('.text4');
+function copy4() {
+    navigator.clipboard.writeText(text4.innerText);
+
+    copyBtn4.innerText = 'Copied';
+    text4.classList.add('copied');
+
+    setTimeout(() => {
+        text4.classList.remove('copied');
+        copyBtn4.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn5 = document.querySelector('.copy-btn5');
+const text5 = document.querySelector('.text5');
+function copy5() {
+    navigator.clipboard.writeText(text5.innerText);
+
+    copyBtn5.innerText = 'Copied';
+    text5.classList.add('copied');
+
+    setTimeout(() => {
+        text5.classList.remove('copied');
+        copyBtn5.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn6 = document.querySelector('.copy-btn6');
+const text6 = document.querySelector('.text6');
+function copy6() {
+    navigator.clipboard.writeText(text6.innerText);
+
+    copyBtn6.innerText = 'Copied';
+    text6.classList.add('copied');
+
+    setTimeout(() => {
+        text6.classList.remove('copied');
+        copyBtn6.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn7 = document.querySelector('.copy-btn7');
+const text7 = document.querySelector('.text7');
+function copy7() {
+    navigator.clipboard.writeText(text7.innerText);
+
+    copyBtn7.innerText = 'Copied';
+    text7.classList.add('copied');
+
+    setTimeout(() => {
+        text7.classList.remove('copied');
+        copyBtn7.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn8 = document.querySelector('.copy-btn8');
+const text8 = document.querySelector('.text8');
+function copy8() {
+    navigator.clipboard.writeText(text8.innerText);
+
+    copyBtn8.innerText = 'Copied';
+    text8.classList.add('copied');
+
+    setTimeout(() => {
+        text8.classList.remove('copied');
+        copyBtn8.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn9 = document.querySelector('.copy-btn9');
+const text9 = document.querySelector('.text9');
+function copy9() {
+    navigator.clipboard.writeText(text9.innerText);
+
+    copyBtn9.innerText = 'Copied';
+    text9.classList.add('copied');
+
+    setTimeout(() => {
+        text9.classList.remove('copied');
+        copyBtn9.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn10 = document.querySelector('.copy-btn10');
+const text10 = document.querySelector('.text10');
+function copy10() {
+    navigator.clipboard.writeText(text10.innerText);
+
+    copyBtn10.innerText = 'Copied';
+    text10.classList.add('copied');
+
+    setTimeout(() => {
+        text10.classList.remove('copied');
+        copyBtn10.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn11 = document.querySelector('.copy-btn11');
+const text11 = document.querySelector('.text11');
+function copy11() {
+    navigator.clipboard.writeText(text11.innerText);
+
+    copyBtn11.innerText = 'Copied';
+    text11.classList.add('copied');
+
+    setTimeout(() => {
+        text11.classList.remove('copied');
+        copyBtn11.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn12 = document.querySelector('.copy-btn12');
+const text12 = document.querySelector('.text12');
+function copy12() {
+    navigator.clipboard.writeText(text12.innerText);
+
+    copyBtn12.innerText = 'Copied';
+    text12.classList.add('copied');
+
+    setTimeout(() => {
+        text12.classList.remove('copied');
+        copyBtn12.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn13 = document.querySelector('.copy-btn13');
+const text13 = document.querySelector('.text13');
+function copy13() {
+    navigator.clipboard.writeText(text13.innerText);
+
+    copyBtn13.innerText = 'Copied';
+    text13.classList.add('copied');
+
+    setTimeout(() => {
+        text13.classList.remove('copied');
+        copyBtn13.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn14 = document.querySelector('.copy-btn14');
+const text14 = document.querySelector('.text14');
+function copy14() {
+    navigator.clipboard.writeText(text14.innerText);
+
+    copyBtn14.innerText = 'Copied';
+    text14.classList.add('copied');
+
+    setTimeout(() => {
+        text14.classList.remove('copied');
+        copyBtn14.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn15 = document.querySelector('.copy-btn15');
+const text15 = document.querySelector('.text15');
+function copy15() {
+    navigator.clipboard.writeText(text15.innerText);
+
+    copyBtn15.innerText = 'Copied';
+    text15.classList.add('copied');
+
+    setTimeout(() => {
+        text15.classList.remove('copied');
+        copyBtn15.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn16 = document.querySelector('.copy-btn16');
+const text16 = document.querySelector('.text16');
+function copy16() {
+    navigator.clipboard.writeText(text16.innerText);
+
+    copyBtn16.innerText = 'Copied';
+    text16.classList.add('copied');
+
+    setTimeout(() => {
+        text16.classList.remove('copied');
+        copyBtn16.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn17 = document.querySelector('.copy-btn17');
+const text17 = document.querySelector('.text17');
+function copy17() {
+    navigator.clipboard.writeText(text17.innerText);
+
+    copyBtn17.innerText = 'Copied';
+    text17.classList.add('copied');
+
+    setTimeout(() => {
+        text17.classList.remove('copied');
+        copyBtn17.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn18 = document.querySelector('.copy-btn18');
+const text18 = document.querySelector('.text18');
+function copy18() {
+    navigator.clipboard.writeText(text18.innerText);
+
+    copyBtn18.innerText = 'Copied';
+    text18.classList.add('copied');
+
+    setTimeout(() => {
+        text18.classList.remove('copied');
+        copyBtn18.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn19 = document.querySelector('.copy-btn19');
+const text19 = document.querySelector('.text19');
+function copy19() {
+    navigator.clipboard.writeText(text19.innerText);
+
+    copyBtn19.innerText = 'Copied';
+    text19.classList.add('copied');
+
+    setTimeout(() => {
+        text19.classList.remove('copied');
+        copyBtn19.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn20 = document.querySelector('.copy-btn20');
+const text20 = document.querySelector('.text20');
+function copy20() {
+    navigator.clipboard.writeText(text20.innerText);
+
+    copyBtn20.innerText = 'Copied';
+    text20.classList.add('copied');
+
+    setTimeout(() => {
+        text20.classList.remove('copied');
+        copyBtn20.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn21 = document.querySelector('.copy-btn21');
+const text21 = document.querySelector('.text21');
+function copy21() {
+    navigator.clipboard.writeText(text21.innerText);
+
+    copyBtn21.innerText = 'Copied';
+    text21.classList.add('copied');
+
+    setTimeout(() => {
+        text21.classList.remove('copied');
+        copyBtn21.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn22 = document.querySelector('.copy-btn22');
+const text22 = document.querySelector('.text22');
+function copy22() {
+    navigator.clipboard.writeText(text22.innerText);
+
+    copyBtn22.innerText = 'Copied';
+    text22.classList.add('copied');
+
+    setTimeout(() => {
+        text22.classList.remove('copied');
+        copyBtn22.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn23 = document.querySelector('.copy-btn23');
+const text23 = document.querySelector('.text23');
+function copy23() {
+    navigator.clipboard.writeText(text23.innerText);
+
+    copyBtn23.innerText = 'Copied';
+    text23.classList.add('copied');
+
+    setTimeout(() => {
+        text23.classList.remove('copied');
+        copyBtn23.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn24 = document.querySelector('.copy-btn24');
+const text24 = document.querySelector('.text24');
+function copy24() {
+    navigator.clipboard.writeText(text24.innerText);
+
+    copyBtn24.innerText = 'Copied';
+    text24.classList.add('copied');
+
+    setTimeout(() => {
+        text24.classList.remove('copied');
+        copyBtn24.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn25 = document.querySelector('.copy-btn25');
+const text25 = document.querySelector('.text25');
+function copy25() {
+    navigator.clipboard.writeText(text25.innerText);
+
+    copyBtn25.innerText = 'Copied';
+    text25.classList.add('copied');
+
+    setTimeout(() => {
+        text25.classList.remove('copied');
+        copyBtn25.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn26 = document.querySelector('.copy-btn26');
+const text26 = document.querySelector('.text26');
+function copy26() {
+    navigator.clipboard.writeText(text26.innerText);
+
+    copyBtn26.innerText = 'Copied';
+    text26.classList.add('copied');
+
+    setTimeout(() => {
+        text26.classList.remove('copied');
+        copyBtn26.innerText = 'Copy';
+    }, 1000);
+}
+
+const copyBtn27 = document.querySelector('.copy-btn27');
+const text27 = document.querySelector('.text27');
+function copy27() {
+    navigator.clipboard.writeText(text27.innerText);
+
+    copyBtn27.innerText = 'Copied';
+    text27.classList.add('copied');
+
+    setTimeout(() => {
+        text27.classList.remove('copied');
+        copyBtn27.innerText = 'Copy';
+    }, 1000);
+}
+
+
+copyBtn1.addEventListener('click', copy1);
+copyBtn2.addEventListener('click', copy2);
+copyBtn3.addEventListener('click', copy3);
+copyBtn4.addEventListener('click', copy4);
+copyBtn5.addEventListener('click', copy5);
+copyBtn6.addEventListener('click', copy6);
+copyBtn7.addEventListener('click', copy7);
+copyBtn8.addEventListener('click', copy8);
+copyBtn9.addEventListener('click', copy9);
+copyBtn10.addEventListener('click', copy10);
+copyBtn11.addEventListener('click', copy11);
+copyBtn12.addEventListener('click', copy12);
+copyBtn13.addEventListener('click', copy13);
+copyBtn14.addEventListener('click', copy14);
+copyBtn15.addEventListener('click', copy15);
+copyBtn16.addEventListener('click', copy16);
+copyBtn17.addEventListener('click', copy17);
+copyBtn18.addEventListener('click', copy18);
+copyBtn19.addEventListener('click', copy19);
+copyBtn20.addEventListener('click', copy20);
+copyBtn21.addEventListener('click', copy21);
+copyBtn22.addEventListener('click', copy22);
+copyBtn23.addEventListener('click', copy23);
+copyBtn24.addEventListener('click', copy24);
+copyBtn25.addEventListener('click', copy25);
+copyBtn26.addEventListener('click', copy26);
+copyBtn27.addEventListener('click', copy27);
