8c5583b5e63aae118d6497e91dcb79a3310ccb10
funnyfoodguy <funnyfoodguy@gmail.com>
2026-01-14 03:36:22 -0500

    Fix

--- js/init.js
+++ js/init.js
@@ -2,3 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
+
+document.addEventListener("click", function() {
+  document.body.style.overflow = 'hidden';
+}, false);
+
+document.addEventListener("touchcancel", function() {
+  document.body.style.overflow = 'hidden';
+}, false);
